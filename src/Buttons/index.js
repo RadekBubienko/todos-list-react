d1dbe7fd0a797edcc78959d71e597aad977f48c8
Radek Bubienko <radek@bubienko.pl>
2022-11-13 16:29:28 +0100

    added to Buttons styled

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,19 +1,19 @@
-import "./style.css";
+import { StyledButtons, Button } from "./styled";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
   tasks.length > 0 && (
-    <div className="buttons">
-      <button onClick={toggleHideDone} className="buttons__button">
+    <StyledButtons>
+      <Button 
+        onClick={toggleHideDone}>
         {hideDone ? "Pokaż" : "Ukryj"} ukończone
-      </button>
-      <button
+      </Button>
+      <Button
         onClick={setAllDone}
-        className="buttons__button"
         disabled={tasks.every(({ done }) => done)}
       >
         Ukończ wszystkie
-      </button>
-    </div>
+      </Button>
+    </StyledButtons>
   )
 );
 
---
+++ src/Buttons/styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const StyledButtons = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+
+    @media (max-width: 767px) {
+        flex-basis: 100%;
+        margin: 10px;
+    }
+`;
+
+export const Button = styled.button`
+  background: transparent;
+    color: #008080;
+    border: none;
+    margin: 0 0 0 20px;
+    transition: color 0.3s;
+    
+    &:hover {
+        color: #008080dd; 
+    }
+
+    &:disabled {
+        color: #ccc;
+    }
+`;
+

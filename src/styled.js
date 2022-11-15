97ac35a1ad733b6ebd6594e2eae9e2cb87c8156e
Radek Bubienko <radek@bubienko.pl>
2022-11-15 17:15:10 +0100

    created styled.js, import export and deleted index.css from src

--- src/styled.js
+++ src/styled.js
@@ -1,3 +1,7 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
 html {
   box-sizing: border-box;
   font-family: 'Montserrat', sans-serif;
@@ -18,6 +22,7 @@ html {
 
 @media (max-width: 767px) {
   .body {
-      max-width: 90%;
+      max-width: 94%;
   }
 }
+`;

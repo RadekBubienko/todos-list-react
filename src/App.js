310da5b8860168d4f5e872519ca9949429f84315
Radek Bubienko <radek@bubienko.pl>
2022-11-24 23:36:37 +0100

    application split into feautures

--- src/App.js
+++ src/App.js
@@ -1,56 +1,5 @@
-import Form from "./Form";
-import Tasks from "./Tasks";
-import Buttons from "./Buttons";
-import Section from "./Section";
-import Header from "./Header";
-import { useState } from "react";
-import { useTasks } from "./useTasks";
+import Tasks from "./features/tasks/Tasks"
 
-function App() {
-  const [hideDone, setHideDone] = useState(false);
-
-  const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
-  };
-
-  
-
-  const {
-    tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-  } = useTasks();
-
-  return (
-    <>
-      <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
-      />
-      <Section
-        title="Lista Zadań"
-        body={
-          <Tasks
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
-        }
-      />
-    </>
-  );
-};
+const App = () => <Tasks />;
 
 export default App;
--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,6 +1,6 @@
 import { List, Item, Content, ButtonDone, ButtonRemove } from "./styled";
 
-const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <List>
     {tasks.map(task => (
       <Item
@@ -26,4 +26,4 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   </List>
 );
 
-export default Tasks;
+export default TaskList;
